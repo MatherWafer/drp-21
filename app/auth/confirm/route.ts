477e2594d8c794564e7d4713050d2d51896ddf11
ayaan <ayaanmather@hotmail.com>
2025-06-04 14:19:28 +0100

    [aam223]fix: edited url in confirm

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -3,9 +3,8 @@ import { type NextRequest } from 'next/server'
 
 import { createClient } from '../../../utils/supabase/server'
 import { redirect } from 'next/navigation'
-
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
+  const { searchParams } = new URL(request.url, 'http://localhost')
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/'
@@ -18,11 +17,9 @@ export async function GET(request: NextRequest) {
       token_hash,
     })
     if (!error) {
-      // redirect user to specified redirect URL or root of app
       redirect(next)
     }
   }
 
-  // redirect the user to an error page with some instructions
   redirect('/error')
 }
